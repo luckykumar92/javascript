95cce22c765de53eefbe73e695fb545b5cb069d1
Lucky Kumar <lucky.kumar@lpu.in>
2023-12-12 11:24:18 +0530

    memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -30,3 +30,27 @@ console.log(typeof bigNumber); //bigint
 console.log(typeof heros); //object
 console.log(typeof myObj); //object
 console.log(typeof myFunction); //function
+
+
+// -------------------------------------------------------
+
+// Stack(Primitive), Heap(Non - Primitive);
+
+let myName = "Lucky";
+let fullName = myName;
+fullName = "Lucky Kumar";
+
+console.log(myName); //Lucky
+console.log(fullName); //Lucky Kumar
+
+let user1 = {
+  email: "user@gmail.com",
+  upi: "user@ybl",
+};
+
+let user2 = user1;
+
+user2.email = "email@gmail.com";
+
+console.log(user1.email); //email@gmail.com
+console.log(user2.email); //email@gmail.com
