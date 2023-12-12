e59d5488e45e00a06fb015a27148eef19dffcbd2
Lucky Kumar <lucky.kumar@lpu.in>
2023-12-12 10:50:07 +0530

    conversion and comparison

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,82 @@
+// let score = 30; //number
+// let score = "33"; //number
+// let score = "33a";//NaN
+// let score = "Lucky"; //NaN
+// let score = null; //0
+// let score = undefined; //NaN
+// let score = true;//1
+// let score = false;//0
+
+// const { score } = req.cody;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// --------------------------------------
+
+// let isLoggedIn = 1; //true;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1 =>true; 0 => false
+// "" => false
+//"Lucky" => true
+
+// --------------------------------------
+
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+
+// console.log(stringNumber); //33;
+// console.log(typeof stringNumber); // string;
+
+// *************************** Operations ************************
+
+// let value = 3;
+// let negValue = -value;
+
+// console.log(negValue); //-3
+// console.log(typeof negValue); //number
+
+// console.log(2 + 2); //4
+// console.log(2 - 2); //0
+// console.log(2 * 2); //4
+// console.log(2 ** 3); //8
+// console.log(2 / 3); //0.6666666666666666
+// console.log(2 % 3); //2
+
+// -----------------------------------------------------
+
+// let str1 = "Lucky";
+// let str2 = "Kumar";
+
+// let str3 = str1 + str2;
+
+// console.log(str3); //LuckyKumar
+// console.log("1" + "2"); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + "2" + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(Number("1") + Number("2")); //3
+
+// //** below code we should not apply
+
+// console.log(true); //true
+// console.log(+true); //1
+// // console.log(true+); //Error
+// console.log(true + 1); //2
+// console.log(+""); //0
+
+// ---------------------------------------
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+
